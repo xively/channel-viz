6e163925eebb1a07bd2d1e9baf64550991005f04
krmped <kmp@kuglerammen.dk>
2013-11-09 18:18:45 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'VxU1u4iV02Ug9iErFW7NI5y7pM76hqzgBroDGuHlTNsjb1Ih', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [97413460], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Sauna Temperatur', // Replaces Xively logo in the header
+		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
