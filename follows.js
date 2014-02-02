0eb98deff2cfee2971a880dc5b0f7f26a9874b6b
walkien <walter.kienle@t-online.de>
2014-02-02 19:56:24 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -148,7 +148,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										renderer: 'line',
+										renderer: 'area',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
