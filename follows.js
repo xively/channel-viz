f1c698ae7560b6083a34baaaf5ac3a86e69f20a5
aqmsunimas <aqmsunimas@gmail.com>
2014-04-29 00:47:45 +0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'wQ9cSgdqgPsaVAremiHhcVpJvDACZNosDH88I8CB4bQAJePS', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1304352703], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'AQMS UNIMAS by Ahmad Faaizi', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
