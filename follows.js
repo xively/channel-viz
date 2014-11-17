52c1ef239375678ccdd72d633862ca23fa8c895e
41D0 <jorge_aido@hotmail.com>
2014-11-17 13:35:21 +0000

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'READ-ONLY', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1,2,3,4], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Monitorização de cubas de vinho', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
